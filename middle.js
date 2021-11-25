426addbad7a692f2e1527637c2bd0799fb110010
Mayu Suzumura <mayumurasuzu@gmail.com>
2021-11-25 08:30:53 -0500

    add middle.js file

--- middle.js
+++ middle.js
@@ -1,12 +1,39 @@
-// const assertEqual = function (actual, expected) {
-//   if (actual !== expected) {
-//     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-//   } else if (actual === expected) {
-//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-//   }
-// };
 
-const person = { firstName: "Khurram" };
-const firstName = person["firstName"];
-const firstNameAgain = person[firstName];
-console.log(firstNameAgain);
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const assertArraysEqual = function (array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: ${JSON.stringify(array1)} === ${JSON.stringify(array2)}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${JSON.stringify(array1)} !== ${JSON.stringify(array2)}`);
+  }
+}
+
+function middle(numbers) {
+  if (numbers.length < 3) {
+    return []
+  }
+  const arrayLengthIsEven = numbers.length % 2 === 0
+  const middleIndex = Math.floor(numbers.length / 2)
+  if (arrayLengthIsEven) {
+    return [numbers[middleIndex - 1], numbers[middleIndex]]
+  }
+  return [numbers[middleIndex]]
+}
+
+assertArraysEqual([], middle([1]))
+assertArraysEqual([], middle([1, 2])); // => []
+assertArraysEqual([2], middle([1, 2, 3])); // => [2]
+assertArraysEqual([3], middle([1, 2, 3, 4, 5])); // => [3]
+assertArraysEqual([2,3], middle([1, 2, 3, 4])); // => [2, 3]
+assertArraysEqual([3, 4], middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
