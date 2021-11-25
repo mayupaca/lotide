0a3af329c3f180cc2d574841d11dae0985c72830
Mayu Suzumura <mayumurasuzu@gmail.com>
2021-11-24 19:19:12 -0500

    add assertArraysEqual file

--- eqArrays.js
+++ eqArrays.js
@@ -15,6 +15,7 @@ const eqArrays = function (array1, array2) {
   return true;
 }
 
+//test
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
