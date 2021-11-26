d023d849219e0c1341481df95ce6e4063ea10085
Mayu Suzumura <mayumurasuzu@gmail.com>
2021-11-25 22:24:18 -0500

    add letterPosition, findKeyByValue file

---
+++ letterPositions.js
@@ -0,0 +1,34 @@
+const eqArrays = function (array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const assertArraysEqual = function (array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }
+}
+
+const letterPositions = function (sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    const letter = sentence[i];
+    if (letter === " ") {
+      continue;
+    }
+    if (results[letter] === undefined) {
+      results[letter] = [i];
+    } else {
+      results[letter].push(i);
+    }
+  }
+  return results;
+};
+
+assertArraysEqual(letterPositions("hello").e, [1]);
