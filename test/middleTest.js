9bcd7a2ce05f46970f20e14952a5a49a338debb2
Mayu Suzumura <mayumurasuzu@gmail.com>
2021-11-29 16:42:30 -0500

    improve tailTest file

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,24 @@
-const assertArraysEqual = require("../assertArraysEqual");
+const assert = require("chai").assert;
 const middle = require("../middle");
 
-assertArraysEqual([], middle([1]))
+describe("#middle", () => {
+  it("returns undefined", () => {
+    assert.deepEqual(middle([]), [1]);
+  });
+  it("returns []", () => {
+    assert.deepEqual(middle([]), [1, 2]);
+  });
+  it("returns [2]", () => {
+    assert.deepEqual(middle([2]), [1, 2, 3]);
+  });
+  it("returns [2, 3]", () => {
+    assert.deepEqual(middle([2, 3]), [1, 2, 3, 4]);
+  });
+});
+
+assertArraysEqual([], middle([1]));
 assertArraysEqual([], middle([1, 2])); // => []
 assertArraysEqual([2], middle([1, 2, 3])); // => [2]
 assertArraysEqual([3], middle([1, 2, 3, 4, 5])); // => [3]
-assertArraysEqual([2,3], middle([1, 2, 3, 4])); // => [2, 3]
+assertArraysEqual([2, 3], middle([1, 2, 3, 4])); // => [2, 3]
 assertArraysEqual([3, 4], middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,18 @@
-const tail = require("../tail");
-const assertEqual = require("../assertEqual");
+// const tail = require("../tail");
+// const assertEqual = require("../assertEqual");
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-const newArray = tail(words);
-assertEqual(newArray.length, 3);
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// const newArray = tail(words);
+// assertEqual(newArray.length, 3);
+
+const assert = require("chai").assert;
+const tail = require("../tail");
+
+describe("#tail", () => {
+  it('returns ["Lighthouse", "Labs"] for ["Yo Yo", "Lighthouse", "Labs"]', () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), (["Lighthouse", "Labs"]));
+  });
+  it("returns [4, 7, 1] for [3, 4, 7, 1]", () => {
+    assert.deepEqual(tail([3, 4, 7, 1]), [4, 7, 1]);
+  });
+});
