06a11d75f9864155c6ecaff36452d08a0e223164
Mayu Suzumura <mayumurasuzu@gmail.com>
2021-11-29 15:16:12 -0500

    created assertArraysEqual test file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,11 +1,12 @@
-const eqArrays = function (array1, array2) {
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
+// const eqArrays = function (array1, array2) {
+//   for (let i = 0; i < array1.length; i++) {
+//     if (array1[i] !== array2[i]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+const eqArrays = require("./eqArrays");
 
 const assertArraysEqual = function (array1,array2) {
   if (eqArrays(array1, array2)) {
@@ -15,7 +16,9 @@ const assertArraysEqual = function (array1,array2) {
   }
 }
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+module.exports = assertArraysEqual;
+
+// assertArraysEqual([1, 2, 3], [1, 2, 3]);
+// assertArraysEqual([1, 2, 3], [3, 2, 1]);
+// assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+// assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
